f8f5590958df318f476ffa777c24e49596b50c80
khusniddin007 <xxusanboyev323@gmail.com>
2022-11-18 09:39:26 +0500

    18 november used Arraylist

--- Bank/src/Customer.java
+++ Bank/src/Customer.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+
 public class Customer {
+    private String customerName;
+    private ArrayList<Double> amountMoney=new ArrayList<>();
+
+    public Customer(String customerName, double amountMoney) {
+        this.customerName = customerName;
+        this.amountMoney.add(amountMoney);
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public ArrayList<Double> getAmountMoney() {
+        return amountMoney;
+    }
+
+
 }
