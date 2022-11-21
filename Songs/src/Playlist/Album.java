83d0da281cb66984bcfae78d1a899df8e981d314
khusniddin007 <xxusanboyev323@gmail.com>
2022-11-21 12:56:41 +0500

    Song hometask is ready

--- Songs/src/Playlist/Album.java
+++ Songs/src/Playlist/Album.java
@@ -1,5 +1,62 @@
 package Playlist;
 
-public class Album {
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+public class Album {
+    private String name;
+    private String artist;
+
+    private ArrayList<Song> songs;
+
+
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs=new ArrayList<>();
+    }
+
+        public boolean addSong(String songTitle,double songDuration){
+        if (finsdSong(songTitle)==null){
+            return songs.add(new Song(songTitle,songDuration));
+        }
+        return false;
+    }
+    public Song finsdSong(String songName){
+
+        for (Song songExsist:
+                songs) {
+            if (songExsist.getTitle().equals(songName)){
+                return songExsist;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlayList(int amountOfSongs, LinkedList<Song> linkedPlayList ){
+        for (int i = 0; i < amountOfSongs; i++) {
+            return linkedPlayList.add(songs.get(i));
+        }
+        return false;
+    }
+    public boolean addToPlayList(String nameOfSong, LinkedList<Song> addViaSongName ){
+
+        if (finsdSong(nameOfSong)!=null){
+               return addViaSongName.add(finsdSong(nameOfSong));
+            }
+
+        return false;
+    }
+    public ArrayList<Song> getSongs() {
+        return songs;
+    }
+
+ /*   @Override
+    private String toString() {
+
+            return name+" : "+artist;
+
+
+      }*/
 }
